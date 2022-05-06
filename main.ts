49918f2f9c88084371819c64e728fcddc81b1608
sas1711006 <105028870+sas1711006@users.noreply.github.com>
2022-05-06 10:15:30 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    servos.P0.run(0)
 })
+input.onButtonPressed(Button.B, function () {
+    servos.P0.run(50)
+})
+servos.P0.setAngle(0)
