03d5d15f7311692274a8035d25393d29049e3e95
sas1711006 <105028870+sas1711006@users.noreply.github.com>
2022-05-30 10:39:15 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-music.setVolume(127)
 basic.forever(function () {
     serial.writeLine("" + (sonar.ping(
     DigitalPin.P1,
@@ -10,11 +9,4 @@ basic.forever(function () {
     DigitalPin.P2,
     PingUnit.Centimeters
     ))
-    if (sonar.ping(
-    DigitalPin.P1,
-    DigitalPin.P2,
-    PingUnit.Centimeters
-    ) > 5) {
-        music.playTone(988, music.beat(BeatFraction.Whole))
-    }
 })
