c2b5990abfd455f3317ffbff37860abb08869315
sas1711006 <105028870+sas1711006@users.noreply.github.com>
2022-05-30 10:55:18 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+music.setVolume(255)
 basic.forever(function () {
     serial.writeLine("" + (sonar.ping(
     DigitalPin.P1,
@@ -9,4 +10,13 @@ basic.forever(function () {
     DigitalPin.P2,
     PingUnit.Centimeters
     ))
+    if (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ) >= 6) {
+        music.playTone(988, music.beat(BeatFraction.Half))
+        basic.pause(200)
+        music.playTone(988, music.beat(BeatFraction.Half))
+    }
 })
